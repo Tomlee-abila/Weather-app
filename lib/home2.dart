2272222d6e87c1eaf77a7cf5e07c27433e8b6dfe
iamtomlee <tomleeabila@gmail.com>
2022-06-09 18:50:04 +0300

    improved the ui

--- lib/home2.dart
+++ lib/home2.dart
@@ -12,6 +12,12 @@ class _homePageState extends State<homePage> {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final widht = MediaQuery.of(context).size.height;
+    var inputBorder = OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30.0),
+                        ),
+                      );
     return Stack(
       children: [
         Positioned(
@@ -19,6 +25,7 @@ class _homePageState extends State<homePage> {
           child: Image.asset(
             'assets/new-york.jpg',
             height: height,
+            fit: BoxFit.fill,
             ),
           ),
         Positioned(
@@ -33,16 +40,43 @@ class _homePageState extends State<homePage> {
           backgroundColor: Colors.black45,
           body: Padding(
             padding: EdgeInsets.symmetric(horizontal: 18),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 50,),
-                Text(
-                  'Hello Siti',
-                  style: TextStyle(fontSize: 100),
+            child: DefaultTextStyle(
+              style: TextStyle(color: Colors.white),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 100,),
+                  Text(
+                    'Hello',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  Text(
+                    'Check the weather by the city',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+
+                  SizedBox(height: 35),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: inputBorder,
+                      enabledBorder: inputBorder,
+                      focusedBorder: inputBorder,
+                      hintText: 'Search city',
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w600
+                      ),
+                      suffixIcon: Icon(Icons.search, color: Colors.white,)
+
+                    ),
+                  ),
+                ]
                 ),
-              ]
-              ),
+            ),
           ),
         ),
       ],
