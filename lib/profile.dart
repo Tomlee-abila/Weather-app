3d5cb6d1c211a148c85da96ff56686b2740c70ba
iamtomlee <tomleeabila@gmail.com>
2022-06-21 10:19:09 +0300

    done with profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,17 +1,253 @@
 import 'package:flutter/material.dart';
 
 
-class Profile extends StatefulWidget {
+
+class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
-  State<Profile> createState() => _ProfileState();
+  Widget build(BuildContext context) {
+    
+    return  profileBodyTwo();
+  }
 }
 
-class _ProfileState extends State<Profile> {
-
+class profileBodyTwo extends StatelessWidget {
+  const profileBodyTwo({Key? key}) : super(key: key);
 
   @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final widht = MediaQuery.of(context).size.height;    
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+
+        //Background Color
+         Positioned(
+          bottom: 0,
+          top: 0,
+          left: 0,
+          right: 0,
+           child: Container(
+            height: height,
+            width: widht,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF99CC99),
+                  Color(0xFFFFCC00),
+                  Color(0xFF99CCFF),
+                  
+                  // Color(0xFF003399),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+                   ),
+         ),
+        
+
+        // ProfileImage
+        Positioned(
+          top: 80,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Container(
+              width: widht-10,
+              height: widht/3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(143, 233, 233, 233),
+                    spreadRadius: 2,
+                  )
+                ],
+                image: DecorationImage(                
+                  image: NetworkImage('https://i.pravatar.cc/300'),
+                  fit: BoxFit.cover,                    
+                  ),
+              ),
+            ),
+          ),
+        ),
+
+        //Name
+        Positioned(
+          top: 360,
+          left: 10,
+          
+          child: Container(
+                  width: 330,                     
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(215, 255, 255, 255),
+                    border: Border.all(
+                      color: Color.fromARGB(107, 255, 255, 255),                         
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: ListTile(
+                    leading: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(0),
+                          image: DecorationImage(                
+                            image: AssetImage('assets/name.png'),
+                            fit: BoxFit.cover,                    
+                            ),
+                        ),
+                      ),
+                    
+                    title: Text(
+                      style: TextStyle(color: Color.fromARGB(189, 5, 168, 5)),
+                      'Name',                          
+                      ),
+                    subtitle: Text(
+                      style: TextStyle(color: const Color.fromARGB(255, 90, 90, 90), fontSize: 20),                          
+                      'Tomlee Osbon Otieno'),
+                  ),
+                ),
+        ),
+        
+        //Birthday
+        Positioned(
+          top: 450,
+          left: 10,
+          
+          child: Container(
+                  width: 330,                     
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(215, 255, 255, 255),
+                    border: Border.all(
+                      color: Color.fromARGB(107, 255, 255, 255),                         
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: ListTile(
+                    leading: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(0),
+                          image: DecorationImage(                
+                            image: AssetImage('assets/birthday.png'),
+                            fit: BoxFit.cover,                    
+                            ),
+                        ),
+                      ),
+                    
+                    title: Text(
+                      style: TextStyle(color: Color.fromARGB(189, 5, 168, 5)),
+                      'Birthday',                          
+                      ),
+                    subtitle: Text(
+                      style: TextStyle(color: const Color.fromARGB(255, 90, 90, 90), fontSize: 20),                          
+                      'May 02, 1997'),
+                  ),
+                ),
+        ),
+        
+
+        //Phone Number
+        Positioned(
+          top: 540,
+          left: 10,
+          
+          child: Container(
+                  width: 330,                     
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(215, 255, 255, 255),
+                    border: Border.all(
+                      color: Color.fromARGB(107, 255, 255, 255),                         
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: ListTile(
+                    leading: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(0),
+                          image: DecorationImage(                
+                            image: AssetImage('assets/phone.png'),
+                            fit: BoxFit.cover,                    
+                            ),
+                        ),
+                      ),
+                    
+                    title: Text(
+                      style: TextStyle(color: Color.fromARGB(189, 5, 168, 5)),
+                      'Phone Number',                          
+                      ),
+                    subtitle: Text(
+                      style: TextStyle(color: const Color.fromARGB(255, 90, 90, 90), fontSize: 20),                          
+                      '+254700565647'),
+                  ),
+                ),
+        ),
+        
+        //email
+        Positioned(
+          top: 630,
+          left: 10,
+          
+          child: Container(
+                  width: 330,                     
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(215, 255, 255, 255),
+                    border: Border.all(
+                      color: Color.fromARGB(107, 255, 255, 255),                         
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: ListTile(
+                    leading: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(0),
+                          image: DecorationImage(                
+                            image: AssetImage('assets/email.png'),
+                            fit: BoxFit.cover,                    
+                            ),
+                        ),
+                      ),
+                    
+                    title: Text(
+                      style: TextStyle(color: Color.fromARGB(189, 5, 168, 5)),
+                      'Email',                          
+                      ),
+                    subtitle: Text(
+                      style: TextStyle(color: const Color.fromARGB(255, 90, 90, 90), fontSize: 20),                          
+                      'tomleeabila@gmail.com'),
+                  ),
+                ),
+        ),
+        
+      ],
+    );   
+  
+  }
+}
+ 
+
+class profileBodyOne extends StatefulWidget {
+  const profileBodyOne({Key? key}) : super(key: key);
+
+  @override
+  State<profileBodyOne> createState() => _profileBodyOneState();
+}
+
+class _profileBodyOneState extends State<profileBodyOne> {
+   @override
   Widget build(BuildContext context) {
 
     return SafeArea(
